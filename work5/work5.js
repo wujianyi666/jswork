befca980075a4a3f3cf094bc1594ce60a7c100da
wujianyi666 <1101370432@qq.com>
2019-10-17 11:41:35 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,5 +1,14 @@
 //9X9的空表，请自行编写这段代码
-
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=9;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str
 
 
 //倒九九乘法空表
@@ -15,3 +24,13 @@ str += '</table>'
 document.getElementById('table2').innerHTML = str
 
 //倒九九乘法空表，请自行编写这段代码
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>' + j + '*' + i + '=' + ( j * i ) + '<td>';
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
