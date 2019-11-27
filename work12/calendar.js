05cae93232f09a2c332eb94513dc61161d377727
Thatitnice <487380346@qq.com>
2019-11-27 21:30:47 +0800

    work12

--- work12/calendar.js
+++ work12/calendar.js
@@ -2,14 +2,16 @@
 function calendar(y){
     //获取指定年份1月1日的星期数值
     var w =new Date(y,0).getDay();
+
     var html ='<div class="box">';
+    
     //拼接每个月份的表格
     for (var m = 1; m<=12;++m){
         html +='<table>';
         html +='<tr class="title"><th colspan="7">' + y + '年' + m + '月</th></tr>';
         html += '<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
-        //获取月份m共有多少天
-        
+       
+        //获取月份m共有多少天 
         var max =new Date(y,m,0).getDate();
 
         html += '<tr>';//开始<tr>标签
