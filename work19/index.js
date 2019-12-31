f064c81db1800c609d38e22c94ac26ad390fab7f
Thatitnice <487380346@qq.com>
2019-12-31 16:01:46 +0800

    work20

--- work19/index.js
+++ work19/index.js
@@ -1,17 +1,34 @@
 let ws
-function connect(){
+function connect() {
     let server = document.getElementById('server').value
     ws = new WebSocket(server);
     ws.onopen = function(){
         document.getElementById('conn').disabled='disable';
         document.getElementById('disconn').disabled='';
         let nickname=document.getElementById('nickname').value
-        if(nickname){
+        if (nickname) {
             ws.send('nickname|'+nickname)
         }
+    }
+    ws.onclose = function () {
+        document.getElementById('conn').disable = '';
+        document.getElementById('disconn').disable = 'disable';
+    }
+    ws.onmessage = function (event) {
+        let board = document.getElementById('board')
+        let newmsg = document.createElement('div')
+        console.log(event.data)
+        newmsg.innerHTML = event.data
+        board.appendChild(newmsg)
+        board.scrollTop = board.scrollHeight;
+    }
+}
 
-    }
-    ws.onclose = function(){
-        
-    }
+function disconnect() {
+    ws.close()
+}
+
+function send() {
+    let msg = document.getElementById('content').value
+    ws.send(msg)
 }
